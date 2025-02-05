98e75544c0026a1d53daa79e0169ebab342b8b84
Jesslyn Caroline <158556535+Earlade@users.noreply.github.com>
2025-02-05 20:27:40 +0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from './App.tsx';
+
+const root = createRoot(document.getElementById('root'));
+root.render({
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
+})
